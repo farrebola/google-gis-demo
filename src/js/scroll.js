ffe1d672f9df1f524508a14cf5f0ed3f4cfcb123
Luis Román <luisromangz@gmail.com>
2014-02-03 13:51:29 +0100

    Changes to the infinite scroll libray to support setting the directive in an element and detect when it becomes visible in its parent.

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,8 +2,8 @@ angular.module('scroll', []).directive('whenScrolled', function() {
     return function(scope, elm, attr) {
         var raw = elm[0];
         
-        elm.bind('scroll', function() {
-            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+        elm.parent().bind('scroll', function() {
+            if (raw.offsetTop + raw.offsetHeight <= raw.parentNode.scrollTop+raw.parentNode.offsetHeight) {
                 scope.$apply(attr.whenScrolled);
             }
         });
