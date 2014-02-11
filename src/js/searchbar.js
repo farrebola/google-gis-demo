622d60d2a8a29234197f1fe9563e96f53ce054ed
Luis Román <luisromangz@gmail.com>
2014-02-11 11:56:37 +0100

    #95092: Improvements to the search bar. We can now fit the view's bounds, clear the search box and go to the selected result if we move the view.

--- src/js/searchbar.js
+++ src/js/searchbar.js
@@ -1,35 +1,28 @@
 googleGisDemo.controller("searchbarCtrl", function($scope, $http) {
+  $scope.loadingPlaces = false;
   $scope.selected = undefined;
   $scope.getLocation = function(val) {
+    $scope.loadingPlaces =true;
     return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
       params: {
-        address: val,
+        address: val+", Greater London, UK",
         bounds: new google.maps.LatLngBounds(
           new google.maps.LatLng(51.280430, -0.563160),
           new google.maps.LatLng(51.683979, 0.278970)),
         region: "gb",
+        language:"en_GB",
         sensor: false
       }
     }).then(function(res){
-      var addresses = [];
-      angular.forEach(res.data.results, function(item){
-        var viewport = new google.maps.LatLngBounds(
-          new google.maps.LatLng(51.280430, -0.563160),
-          new google.maps.LatLng(51.683979, 0.278970));
-        if(viewport.contains(
-          new google.maps.LatLng(
-            item.geometry.location.lat, 
-            item.geometry.location.lng))){
-          addresses.push(item);
-        }
-      });
-      return addresses;
+      $scope.loadingPlaces =false;
+      return res.data.results;
     });
   };
   $scope.select = function(val){
-    $scope.map.panTo(val.geometry.location);
-    $scope.map.setZoom($scope.map.zoom + 10);
-    marker = new google.maps.Marker({
+    $scope.selectedResult = val;
+    $scope._centerView(val.geometry);
+    
+    var marker = new google.maps.Marker({
       position: val.geometry.location,
       title: val.formatted_address,
       clickable: true,
@@ -47,6 +40,25 @@ googleGisDemo.controller("searchbarCtrl", function($scope, $http) {
         marker.showInfoWindow();
     });
   };
+  
+  $scope._centerView = function(geometry) {
+    if(geometry.bounds) {
+      $scope.map.fitBounds(new google.maps.LatLngBounds(
+        new google.maps.LatLng(geometry.bounds.southwest.lat,geometry.bounds.southwest.lng),
+        new google.maps.LatLng(geometry.bounds.northeast.lat,geometry.bounds.northeast.lng)));  
+    } else {
+      $scope.map.panTo(geometry.location);        
+    }
+  };
+  
+  $scope.clearSearch = function() {
+    $scope.selected = null;    
+  };
+  
+  $scope.goTo = function() {
+     $scope._centerView($scope.selectedResult.geometry);
+  };
+  
   google.maps.event.addDomListener(window, 'load', function(){
     $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push($('.input-group')[0]);
     $('.input-group')[0].style.removeProperty("display");
